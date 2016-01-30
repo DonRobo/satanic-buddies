cf6da708caaa304289be19a70df846fc54789563
Samuel Kogler <samuel.kogler@gmail.com>
2016-01-30 11:30:54 +0100

    animation improvements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,8 +3,17 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+    enum AnimationState
+    {
+        IDLE = 0,
+        WALK
+    }
 
     public float walkSpeed = 300.0f;
+
+    public float animationSpeedFactor = 30.0f;
+
+    public float zMovementAnimationSpeedFactor = 0.5f;
 
     // Maximum Z deviation that the player can have
     public float worldZLimit = 1.0f;
@@ -13,34 +22,53 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 moveDirection = Vector3.zero;
     private Vector3 initialPosition;
-    private CharacterController controller;
+    private CharacterController characterController;
+    private Animator animator;
+    private GameObject spriteView;
 
-    void Start ()
+    void Start()
     {
         initialPosition = transform.position;
-        controller = GetComponent<CharacterController>();
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+        spriteView = transform.FindChild("PlayerSpriteView").gameObject;
     }
-	
-	void Update ()
+
+    void Update()
     {
-        if (controller.isGrounded)
+        if (characterController.isGrounded)
         {
-            
-            float translationZ = Input.GetAxis("Vertical") * walkSpeed;
             float translationX = Input.GetAxis("Horizontal") * walkSpeed;
+            float translationZ = Input.GetAxis("Vertical") * walkSpeed;
 
-            float newZ = transform.position.z + translationZ;
+            translationZ = Mathf.Clamp(transform.position.z + translationZ,
+                initialPosition.z - worldZLimit, initialPosition.z + worldZLimit);
 
-            newZ = Mathf.Clamp(newZ, initialPosition.z - worldZLimit, initialPosition.z + worldZLimit);
-
-            moveDirection = new Vector3(translationX, 0, newZ - transform.position.z);
+            moveDirection = new Vector3(translationX, 0, translationZ - transform.position.z);
             moveDirection = transform.TransformDirection(moveDirection);
-      
-           if (Input.GetButton("Jump"))
-               moveDirection.y = 6.0f;
-
         }
+
+        animator.speed = (Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.z) * zMovementAnimationSpeedFactor) * animationSpeedFactor;
+
         moveDirection.y -= gravity;
-        controller.Move(moveDirection);
+
+        if (Mathf.Abs(moveDirection.x) > 0 || Mathf.Abs(moveDirection.z) > 0)
+        {
+            animator.SetInteger("state", (int)AnimationState.WALK);
+        }
+        else
+        {
+            animator.SetInteger("state", (int)AnimationState.IDLE);
+        }
+        if (moveDirection.x < 0 && spriteView.transform.localScale.x > 0)
+        {
+            spriteView.transform.localScale = new Vector3(-spriteView.transform.localScale.x, spriteView.transform.localScale.y, spriteView.transform.localScale.z);
+        }
+        else if (moveDirection.x > 0 && spriteView.transform.localScale.x < 0)
+        {
+            spriteView.transform.localScale = new Vector3(-spriteView.transform.localScale.x, spriteView.transform.localScale.y, spriteView.transform.localScale.z);
+        }
+
+        characterController.Move(moveDirection);
     }
 }
