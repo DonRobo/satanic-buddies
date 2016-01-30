95f783a64dc1e030d0d59c692c6613e101024fa7
Robert Tieber <robert.tieber@gmail.com>
2016-01-30 11:10:18 +0100

    Added obstacles

--- Assets/Scripts/Levelspawner.cs
+++ Assets/Scripts/Levelspawner.cs
@@ -1,25 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class Levelspawner : MonoBehaviour {
-    
+public class Levelspawner : MonoBehaviour
+{
+
     public GameObject levelPlane;
     public GameObject player;
     public int lastTile = -1;
+
     private readonly int tileSize = 100;
 
-    void Start () {
+    private GameObject[] obstacleLib;
+
+    void Start()
+    {
         if (player == null)
         {
             player = GameObject.Find("Player");
         }
+        obstacleLib = Resources.LoadAll<GameObject>("Obstacles");
     }
 
-    void Update () {
+    void Update()
+    {
         if (player.transform.position.x > lastTile * tileSize - tileSize / 2)
         {
-            GameObject.Instantiate(levelPlane, new Vector3( lastTile * tileSize+tileSize/2,0,0), Quaternion.identity);
+            GameObject.Instantiate(levelPlane, new Vector3(lastTile * tileSize + tileSize / 2, 0, 0), Quaternion.identity);
+            AddRandomObstacles();
+            player.transform.position = new Vector3(player.transform.position.x, 0, player.transform.position.z);
             lastTile++;
         }
-	}
+    }
+
+    private void AddRandomObstacles()
+    {
+        float minX = lastTile * tileSize;
+        float minY = -10;
+        float maxX = minX + tileSize;
+        float maxY = 10;
+
+        for (int i = 0; i < 30; i++)
+        {
+            GameObject obstacle = obstacleLib[UnityEngine.Random.Range(0, obstacleLib.Length)];
+            //Debug.Log(new Vector3(UnityEngine.Random.Range(minX, maxX), 0, UnityEngine.Random.Range(minY, maxY)));
+            GameObject.Instantiate(obstacle, new Vector3(UnityEngine.Random.Range(minX, maxX), 0, UnityEngine.Random.Range(minY, maxY)), Quaternion.identity);
+        }
+    }
 }
