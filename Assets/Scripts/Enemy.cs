a9da1288d78eb9a0a3a53fec93b47ee71fc6f89c
Robert Tieber <robert.tieber@gmail.com>
2016-01-30 15:15:29 +0100

    Tweaked enemies to be more mobby

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,29 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
 
+    public float speed = 10;
     private GameObject player;
     private float health = 100;
 
     // Use this for initialization
-    void Start () {
-	    if(player== null)
+    void Start()
+    {
+        if (player == null)
         {
             player = GameObject.Find("Player");
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        float step = Time.deltaTime * 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 distance = (player.transform.position - transform.position);
+        float speedModifier = distance.sqrMagnitude > 100 ? 1.1f : 1f;
+        float step = Time.deltaTime * speed * speedModifier;
         float oldY = transform.position.y;
-        Vector3 movement = (player.transform.position- transform.position).normalized * step;
+        Vector3 movement = distance.normalized * step;
         transform.Translate(movement);
-      //  transform.position= Vector3.MoveTowards(transform.position, player.transform.position, step);
-       // transform.position = new Vector3(transform.position.x, oldY, transform.position.z);
+        //  transform.position= Vector3.MoveTowards(transform.position, player.transform.position, step);
+        // transform.position = new Vector3(transform.position.x, oldY, transform.position.z);
         //this.GetComponent<CharacterController>().Move(new Vector3(0.01f, 0, 0.01f));
-	}
+    }
 
     public void Damage(float damage)
     {
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,25 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Spawner : MonoBehaviour {
+public class Spawner : MonoBehaviour
+{
 
     public GameObject prefab;
-    public float interval=1;
+    public float interval = 1;
 
-    private float lastSpawn=0;
+    private float lastSpawn = 0;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         lastSpawn += Time.deltaTime;
         if (lastSpawn > interval)
         {
             lastSpawn = 0;
-            GameObject.Instantiate(prefab, transform.position, Quaternion.identity);
+            GameObject.Instantiate(prefab, transform.position + new Vector3(0, 0, Random.Range(-20, 20)), Quaternion.identity);
         }
-	}
+    }
 }
