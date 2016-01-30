d076120dc56146eeb6f181bb3ebcb2d1b006511f
Robert Tieber <robert.tieber@gmail.com>
2016-01-30 10:27:48 +0100

    Added infinite level spawning

---
+++ Assets/Scripts/Levelspawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Levelspawner : MonoBehaviour {
+    
+    public GameObject levelPlane;
+    public GameObject player;
+    public int lastTile = -1;
+    private readonly int tileSize = 100;
+
+    void Start () {
+        if (player == null)
+        {
+            player = GameObject.Find("Player");
+        }
+    }
+
+    void Update () {
+        if (player.transform.position.x > lastTile * tileSize - tileSize / 2)
+        {
+            GameObject.Instantiate(levelPlane, new Vector3( lastTile * tileSize+tileSize/2,0,0), Quaternion.identity);
+            lastTile++;
+        }
+	}
+}
