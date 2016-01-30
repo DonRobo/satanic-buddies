5426cfe62efe3fb73a98bc7383d2ab0e44b65382
Samuel Kogler <samuel.kogler@gmail.com>
2016-01-30 18:26:57 +0100

    input, comet fix

--- Assets/Scripts/Comet.cs
+++ Assets/Scripts/Comet.cs
@@ -17,17 +17,14 @@ public class Comet : MonoBehaviour {
 		float step = Time.deltaTime * 20;
 		Vector3 movement = (Vector3.down) * step;
 		transform.Translate(movement);
-		//  transform.position= Vector3.MoveTowards(transform.position, player.transform.position, step);
-		// transform.position = new Vector3(transform.position.x, oldY, transform.position.z);
-		//this.GetComponent<CharacterController>().Move(new Vector3(0.01f, 0, 0.01f));
-		//if(Mathf.Abs(transform.position.y) < 0.2){
-		OnTriggerEnter(Collider other) 
-		{
-			//GameObject explosion = GameObject.Instantiate(explosionPrefab, transform.position, Quaternion.identity) as GameObject;
-			//explosion.GetComponent<Explosion>().force = force;
-			//explosion.GetComponent<Explosion>().range = range;
-			//explosion.GetComponent<Explosion>().damage = damage;
-			//Destroy(this.gameObject);
+
+		if(Mathf.Abs(transform.position.y) < 0.2)
+        {
+			GameObject explosion = GameObject.Instantiate(explosionPrefab, transform.position, Quaternion.identity) as GameObject;
+			explosion.GetComponent<Explosion>().force = force;
+			explosion.GetComponent<Explosion>().range = range;
+			explosion.GetComponent<Explosion>().damage = damage;
+			Destroy(this.gameObject);
 		}
 	}
 }
